e6ed97fe542ec5e236d4b5f42fa5ef247ca78e3f
Scott Przybylski <skaughtx0r@gmail.com>
2016-05-25 11:55:31 -0700

    Changing package name back

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_version():
 
 setup(name='mocu_vcstools',
       version=get_version(),
-      packages=['mocu_vcstools'],
+      packages=['vcstools'],
       package_dir={'': 'src'},
       scripts=[],
       install_requires=['pyyaml', 'python-dateutil'],
